6d7795faa7194ae97bde43ef6dbb4063b6fb0a27
Bharathgopal <bharathgopal75@gmail.com>
2020-12-26 15:12:54 +0530

    Assignment-2 functions code

--- hello2.c
+++ hello2.c
@@ -1,2 +1,6 @@
 //Something
 //Demo of git tag
+
+/*
+This is a tracked file and I am adding more commnets
+*/
---
+++ test1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+int main()
+{
+	printf("I am in Master Branch");
+	printf("Demo of Git rebase");
+	printf("Edited from github Online Editor");
+	printf("Changes from Developer-1");
+
+	return 0;
+}
+
+void sum(int a ,int b )
+{
+	int c = a+b;
+}
