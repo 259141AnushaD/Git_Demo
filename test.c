531aae06fe131df10df2a7213e60cf2bc52dc837
Bharathgopal <bharathgopal75@gmail.com>
2021-02-02 15:23:50 +0530

    Changes that are made

--- test.c
+++ test.c
@@ -1,15 +1,12 @@
 #include <stdio.h>
 int main()
 {
-	printf("I am in Master Branch");
-	printf("Demo of Git rebase");
-	printf("Edited from github Online Editor");
 	printf("Changes from Developer-1");
-
+    
 	return 0;
 }
 
-void sum(int a ,int b )
+int function()
 {
-	int c = a+b;
+    //do nothing
 }
